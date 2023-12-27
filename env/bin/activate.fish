388566e4d288e56705238006e54218f1d12ffc27
justNeto <continental.lelgg.2b@gmail.com>
2023-12-27 15:22:55 +0100

    Added dependencies

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/neto/projects/flask-MALE-project/env"
+set -gx VIRTUAL_ENV "/home/neto/projects/flask-MALE-deployment/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
