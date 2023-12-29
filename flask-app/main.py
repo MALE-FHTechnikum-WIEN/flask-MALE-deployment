e80f048c12e87e466bd1dd2cd0daecb14600b56d
F15H3Rdu95 <baptiste.lorenzi@edu.esiee-it.fr>
2023-12-29 17:18:22 +0100

    Added a webpage and a skeleton working for Flask

--- flask-app/main.py
+++ flask-app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import subprocess
 
 app = Flask(__name__)
@@ -16,8 +16,18 @@ def predict(value):
 @app.route('/info', methods=['GET'])
 def info():
     # Debugging information
-    return jsonify({'This app was created by:': ['Ernesto', 'Baptiste']})
+    return jsonify({'This app was created by:': ['Ernesto', 'Baptiste Gigachad']})
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET': #When a 'GET' request is pushed, the code below will be executed
+        return render_template('index.html')
+    
+    if request.method == 'POST': #When a 'POST' request is pushed, the code below will be executed
+        #Prediction is the name we gave our input in the html form, the name has to be given to retrieve the correct data
+        prediction = request.form.get('prediction', '')
+        print(prediction)
+        return render_template('index.html', response=prediction)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
