12a03e0f66235e445741b8c5b406aa9103bfe41a
F15H3Rdu95 <baptiste.lorenzi@edu.esiee-it.fr>
2023-12-30 18:11:12 +0100

    merged branch

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@
 FROM python:3.9
 
 RUN apt update && \
-    apt-get install -y bash-completion\
-    sudo tmux xterm nano python3-pip git vim python3-pip --no-install-recommends\
-	&& rm -rf /var/lib/apt/lists/
+    apt-get install -y bash-completion \
+    sudo tmux r-base r-base-dev xterm nano python3-pip git vim python3-pip --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/
 
 ENV USERNAME flask_user
 ARG USER_ID=1000
@@ -15,7 +15,7 @@ RUN groupadd --gid $GROUP_ID $USERNAME && \
         echo "$USERNAME:$USERNAME" | chpasswd && \
         usermod --shell /bin/bash $USERNAME && \
         usermod -aG sudo $USERNAME && \
-        usermod  --uid $USER_ID $USERNAME && \
+        usermod --uid $USER_ID $USERNAME && \
         echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$USERNAME && \
         chmod 0440 /etc/sudoers.d/$USERNAME
 
@@ -27,6 +27,12 @@ COPY requirements.txt /home/$USERNAME/
 
 # Install Python dependencies
 RUN pip install --no-cache-dir -r /home/$USERNAME/requirements.txt
+
+# Set a personal library location for R
+ENV R_LIBS_USER /usr/local/lib/R/site-library
+
+# Install R packages
+RUN R -e 'install.packages("randomForest", repos="https://cran.rstudio.com/")'
 
 # Expose port 5000 for Flask
 EXPOSE 5000
--- flask-app/model.py
+++ flask-app/model.py
@@ -1,11 +1,13 @@
 import rpy2.robjects as robjects
 from rpy2.robjects import conversion, default_converter
 
+
 def run_model(predict_value): 
 #The function that will be called when the 'POST' request will be sent to our Flask Server
 
     with conversion.localconverter(default_converter):
         #robject is the object in which you can write your R code
+
         robjects.r(f'''
                    predict_iris <- function() {{
 
@@ -13,16 +15,12 @@ def run_model(predict_value):
 
                        # Load the iris_model.rds file
                        rf_model <- readRDS("iris_model.rds")
-                       #print('hello')
 
                        # Replace `new_sepal_length` with the actual Sepal.Length value you want to predict
                        new_data_point <- data.frame(Sepal.Length = {predict_value})
 
                        # Make predictions using the trained Random Forest model
                        prediction <- predict(rf_model, newdata = new_data_point)
-
-                       # View the predicted species
-                       #print(prediction)
 
                        return(as.character(prediction))
                        }}
@@ -31,5 +29,3 @@ def run_model(predict_value):
         # Call the R function and get the predictions
         return str(robjects.r('predict_iris()')[0])
 
-
-    
